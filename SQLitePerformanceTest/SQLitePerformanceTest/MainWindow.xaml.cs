5e7b8498329a2937d52463cd3c4bac3fcb6c58e0
atachimiko <atachi.miko@gmail.com>
2014-01-10 03:54:48 +0900

    Initialize Repository

---
+++ SQLitePerformanceTest/SQLitePerformanceTest/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLitePerformanceTest
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			string personalDirectoryPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			AppDomain.CurrentDomain.SetData("DataDirectory", personalDirectoryPath);
+
+			Console.WriteLine("SQLiteデータベースファイルの初期化を行います。");
+			InitializeDatabase();
+			Console.WriteLine("初期化が完了しました。");
+
+			InitializeComponent();
+		}
+
+
+		/// <summary>
+		/// データベースの初期化を行う
+		/// </summary>
+		void InitializeDatabase()
+		{
+			string sqltext = "";
+			System.Reflection.Assembly assm = System.Reflection.Assembly.GetExecutingAssembly();
+			using (var stream = assm.GetManifestResourceStream("SQLitePerformanceTest.Assets.Sql.sqltest.txt"))
+			{
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					sqltext = reader.ReadToEnd();
+				}
+			}
+
+			using (var @dbc = new MyDbContext())
+			{
+				@dbc.Database.ExecuteSqlCommand(sqltext);
+				@dbc.SaveChanges();
+			}
+		}
+	}
+}
---
+++ SQLitePerformanceTest/SQLitePerformanceTest/MyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLitePerformanceTest
+{
+	public class MyDbContext : DbContext
+	{
+		public MyDbContext()
+			: base("sqlitetest")
+		{
+
+		}
+
+		public DbSet<Person> Persons { get; set; }
+	}
+
+	public class Person
+	{
+		public long Id { get; set; }
+		public string Name { get; set; }
+	}
+}
